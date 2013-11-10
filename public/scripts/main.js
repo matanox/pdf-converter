5a3347733b85742a07db9781028234476d407dc3
matanster <matan@otozip.com>
2013-11-10 21:37:42 +0200

    hadasino integration

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,11 +6,37 @@ requirejs.config({
     enforceDefine: false,
     paths: {
         filePicker: 'https://api.filepicker.io/v1/filepicker',
-	jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min'
-    }
-});
+		jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+	}
+   });
 
 require(['filePicker'], function() {
     console.log('connecting to filepicker');
     filepicker.setKey('A98TZEfEaSi6e7ru2EZnxz');   
 });
+
+// TODO: move this under the auspices of require if it gets buggy
+require(['jquery'], function() {
+	$.getScript("./hadasino/socket.io.js").done(function( data, textStatus, jqxhr ) {
+  		/* console.log( data ); // Data returned
+  		console.log( textStatus ); // Success
+  		console.log( jqxhr.status ); // 200 */
+		console.log("hadasino loaded")
+
+		// connect to Hadas server and listen for reload events
+		// need to make sure this runs after at least socket.io was loaded.
+		//
+		var socket = io.connect('http://localhost:1338');		
+		socket.emit('projectClient');		
+		socket.on('clientRecycle', function() {
+			console.log("trying to reload")
+			location.reload(true);
+		});
+	}).fail(function(jqxhr, settings, exception) {
+		console.log("failed loading hadasino:");
+		console.log( jqxhr.status ); 
+		console.log( settings ); 
+		console.log( exception ); 
+	});
+});
+
--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -8,10 +8,10 @@ require(['filePicker','jquery'], function() {
 	    //console.log('in click');
 	    filepicker.pick({mimetypes: ['application/pdf'], container: 'modal', services: ['COMPUTER','URL','DROPBOX','GOOGLE_DRIVE','BOX'], maxSize: 50*1024*1024, debug: false}, 
 		    function(InkBlob){
-    		      console.log(JSON.stringify(InkBlob));
+    		    console.log(JSON.stringify(InkBlob));
  		    },
 		    function(FPError){
-		      console.log(FPError.toString());
+		    	console.log(FPError.toString());
 		    })
         
 	//e.preventDefault();
