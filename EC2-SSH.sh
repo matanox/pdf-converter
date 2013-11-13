347f670fa5ba93c963fc039490b546085bbbf932
matanster <matan@otozip.com>
2013-11-13 21:03:53 +0200

    not much

---
+++ EC2-SSH.sh
@@ -0,0 +1,1 @@
+ssh -i ~/.ssh/EC2-pdf-conversion-server-key-pair.pem  ubuntu@ec2-54-201-48-56.us-west-2.compute.amazonaws.com
---
+++ routes/convert.js
@@ -0,0 +1,49 @@
+getFromUrl = require("request")
+exec = require('child_process').exec;
+fs = require('fs')
+require('stream')
+executable = 'pdf2htmlEX'
+execCommand = executable + ' '
+
+/*
+ * Fetches the upload from Ink File Picker (writing it into local file).
+ * If it works - invoke the passed along callback.
+ */
+function fetch(inkUrl, callback)
+{
+	//outFile = inkUrl + '.pdf';
+	outFile = 'local-copies/upload.pdf'
+	download = getFromUrl(inkUrl, function(error, response, body){
+		if (!error && response.statusCode == 200) 
+			callback(outFile);
+		else
+		{
+			console.log('fetching from InkFilepicker returned http status ' + response.statusCode);
+			if (error)
+			{
+				console.log('fetching from InkFilepicker returned error ' + error);			
+			}
+		}		
+	}).pipe(fs.createWriteStream(outFile));	
+}
+
+exports.go = function(req, res){
+
+	function convert(localCopy)
+	{
+
+		execCommand += localCopy
+		console.log(execCommand)
+
+	  	exec(execCommand, function (error, stdout, stderr) {
+	    	console.log(executable + '\'s stdout: ' + stdout);
+	    	console.log(executable + '\'s stderr: ' + stderr);
+	    	if (error !== null) {
+	      		console.log(executable + '\'sexec error: ' + error);
+		    }
+		});
+		res.send("Please wait...");	
+	}
+
+	fetch(req.query.tempLocation, convert);
+};
