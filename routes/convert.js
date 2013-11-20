cc1c2b1b7614dd11ded0b27f245a708d6cc13028
matanster <matan@otozip.com>
2013-11-20 14:58:07 +0200

    Naive redirect to next level handling

--- routes/convert.js
+++ routes/convert.js
@@ -7,15 +7,15 @@ execCommand = executable + ' '
 
 /*
  * Fetches the upload from Ink File Picker (writing it into local file).
- * If it works - invoke the passed along callback.
+ * If it works - invoke the passed along callback function.
  */
-function fetch(inkUrl, callback)
+function fetch(inkUrl, callOnSuccess)
 {
 	//outFile = inkUrl + '.pdf';
 	outFile = 'local-copies/' + 'pdf/' + inkUrl.replace("https://www.filepicker.io/api/file/","") + '.pdf'
 	download = getFromUrl(inkUrl, function(error, response, body){
 		if (!error && response.statusCode == 200) 
-			callback(outFile);
+			callOnSuccess(outFile);
 		else
 		{
 			console.log('fetching from InkFilepicker returned http status ' + response.statusCode);
@@ -31,6 +31,8 @@ exports.go = function(req, res){
 
 	function convert(localCopy)
 	{
+			
+		//res.send('Please wait...'');	
 
 		execCommand += localCopy += ' ' + '--dest-dir=' + 'local-copies/' + 'html-converted/'
 		console.log(execCommand)
@@ -41,9 +43,23 @@ exports.go = function(req, res){
 	    	if (error !== null) {
 	      		console.log(executable + '\'sexec error: ' + error);
 		    }
+		    else {
+		    	console.log('Passing html result to next level handler')
+/* 
+				 * this long concatentaion is the redirect to the back-end server, 
+				 * with the relative location of the output file. Needs some 
+				 * readability cleanup, if this code survives.
+				 *
+				 */
+				redirectString = 'http://localhost:8080/' + '?' 
+				+ '../front-end/' + 'local-copies/' + 'html-converted/' + 
+				localCopy.replace('local-copies/pdf/', '').replace('.pdf', '') + '.html'
+				    	
+    			res.writeHead(301, {'Location': redirectString});
+		    	res.end();
+		    }
 		});
-		res.send("Please wait...");	
 	}
 
-	fetch(req.query.tempLocation, convert);
+	fetch(req.query.tempLocation, convert); // fetch the upload and pass control to the convert function
 };
