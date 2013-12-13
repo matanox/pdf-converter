3cd31dcd195e66152cffa6c3b6c175ab5a7803bc
matanster <matan@otozip.com>
2013-12-14 01:21:19 +0200

    "1"

--- gitit.sh
+++ gitit.sh
@@ -1,10 +1,11 @@
 color='\e[1;34m'
 detail_color='\e[1;30m'
 NC='\e[0m' # No Color
-echo -e "${detail_color}"
+echo -e ${detail_color}
 git status
 git add .
 git add -u
 git status
+echo -e ${color}
 echo "trying to commit with comment \"$1\""
 git commit -am \"1\"
