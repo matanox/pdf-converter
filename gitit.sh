14f81ac91879f7c344ba3b4f0ecfafa5c5322e78
matanster <matan@otozip.com>
2013-12-14 01:24:21 +0200

    "1"

--- gitit.sh
+++ gitit.sh
@@ -2,13 +2,13 @@ color='\e[1;34m'
 detail_color='\e[1;30m'
 NC='\e[0m' # No Color
 echo -e ${color}
-echo "trying to stage and commit all changes"
+echo "Trying to stage and commit all changes"
 echo -e ${detail_color}
 git status
 git add .
 git add -u
 git status
 echo -e ${color}
-echo "trying to commit with comment \"$1\""
+echo "Trying to commit with comment \"$1\""
 git commit -am \"1\"
 echo -e ${NC}
